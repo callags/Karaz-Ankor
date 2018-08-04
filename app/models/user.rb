58b53ae1deeeb7f29e7c3865ebc9d34e322d7205
Shane Callaghan <s.callaghan513@gmail.com>
2018-08-04 17:44:15 +0000

    Solved devise user creation error

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
 		  :fido_usf_registerable, :fido_usf_authenticatable
+		  
+  attr_accessor :email, :password, :password_confirmation
+  
   has_many :articles
 end

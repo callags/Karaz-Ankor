7a150747d78b4def01d8de38ba129447d589f467
Callags <shane@LAPTOP-KG0LMKD6.localdomain>
2023-09-10 16:24:31 -0400

    Changed Environment.rb file attempt 1

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ require_relative 'application'
 
 # Load the app's custom environment variables here, so that they are loaded before environments/*.rb
 local_env = File.join(Rails.root, 'config', 'local_env.rb')
-load(local_env) if File.exists?(local_env)
+load(local_env) if File.exist?(local_env)
 
 # Initialize the Rails application.
 Rails.application.initialize!
